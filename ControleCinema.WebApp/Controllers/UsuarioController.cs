646fe9f2698c0a3b2836388cabe69f7ff2357130
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-08-13 15:57:24 -0300

    Corrigidas dependências

--- ControleCinema.WebApp/Controllers/UsuarioController.cs
+++ ControleCinema.WebApp/Controllers/UsuarioController.cs
@@ -3,118 +3,117 @@ using ControleCinema.WebApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ControleCinema.WebApp.Controllers
+namespace ControleCinema.WebApp.Controllers;
+
+public class UsuarioController : Controller
 {
-    public class UsuarioController : Controller
+    private readonly UserManager<Usuario> userManager;
+    private readonly SignInManager<Usuario> signInManager;
+    private readonly RoleManager<Perfil> roleManager;
+
+    public UsuarioController(
+        UserManager<Usuario> userManager,
+        SignInManager<Usuario> signInManager,
+        RoleManager<Perfil> roleManager
+    )
     {
-        private readonly UserManager<Usuario> userManager;
-        private readonly SignInManager<Usuario> signInManager;
-        private readonly RoleManager<Perfil> roleManager;
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+        this.roleManager = roleManager;
+    }
 
-        public UsuarioController(
-            UserManager<Usuario> userManager,
-            SignInManager<Usuario> signInManager,
-            RoleManager<Perfil> roleManager
-        )
+    public IActionResult Registrar()
+    {
+        return View(new RegistrarViewModel());
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Registrar(RegistrarViewModel registrarVm)
+    {
+        if (!ModelState.IsValid)
+            return View(registrarVm);
+
+        var usuario = new Usuario()
         {
-            this.userManager = userManager;
-            this.signInManager = signInManager;
-            this.roleManager = roleManager;
-        }
+            UserName = registrarVm.Usuario,
+            Email = registrarVm.Email
+        };
 
-        public IActionResult Registrar()
+        var resultadoCriacaoUsuario =
+            await userManager.CreateAsync(usuario, registrarVm.Senha!);
+
+        var resultadoCriacaoTipoUsuario =
+            await roleManager.FindByNameAsync(registrarVm.Tipo);
+
+        if (resultadoCriacaoTipoUsuario is null)
         {
-            return View(new RegistrarViewModel());
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Registrar(RegistrarViewModel registrarVm)
-        {
-            if (!ModelState.IsValid)
-                return View(registrarVm);
-
-            var usuario = new Usuario()
+            var cargo = new Perfil()
             {
-                UserName = registrarVm.Usuario,
-                Email = registrarVm.Email
+                Name = registrarVm.Tipo,
+                NormalizedName = registrarVm.Tipo!.ToUpper(),
+                ConcurrencyStamp = Guid.NewGuid().ToString()
             };
 
-            var resultadoCriacaoUsuario =
-                await userManager.CreateAsync(usuario, registrarVm.Senha!);
-
-            var resultadoCriacaoTipoUsuario =
-                await roleManager.FindByNameAsync(registrarVm.Tipo);
-
-            if (resultadoCriacaoTipoUsuario is null)
-            {
-                var cargo = new Perfil()
-                {
-                    Name = registrarVm.Tipo,
-                    NormalizedName = registrarVm.Tipo!.ToUpper(),
-                    ConcurrencyStamp = Guid.NewGuid().ToString()
-                };
-
-                await roleManager.CreateAsync(cargo);
-            }
-
-            await userManager.AddToRoleAsync(usuario, registrarVm.Tipo);
-
-            if (resultadoCriacaoUsuario.Succeeded)
-            {
-                await signInManager.SignInAsync(usuario, isPersistent: false);
-
-                return RedirectToAction("Index", "Inicio");
-            }
-
-            foreach (var erro in resultadoCriacaoUsuario.Errors)
-                ModelState.AddModelError(string.Empty, erro.Description);
-
-            return View(registrarVm);
+            await roleManager.CreateAsync(cargo);
         }
 
+        await userManager.AddToRoleAsync(usuario, registrarVm.Tipo);
 
-        public IActionResult Login(string? returnUrl = null)
+        if (resultadoCriacaoUsuario.Succeeded)
         {
-            ViewBag.ReturnUrl = returnUrl;
+            await signInManager.SignInAsync(usuario, isPersistent: false);
 
-            return View();
+            return RedirectToAction("Index", "Inicio");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
-        {
-            ViewBag.ReturnUrl = returnUrl;
+        foreach (var erro in resultadoCriacaoUsuario.Errors)
+            ModelState.AddModelError(string.Empty, erro.Description);
 
-            if (!ModelState.IsValid)
-                return View(model);
+        return View(registrarVm);
+    }
 
-            var resultadoLogin = await signInManager.PasswordSignInAsync(
-                model.Usuario,
-                model.Senha,
-                false,
-                false
-            );
 
-            if (resultadoLogin.Succeeded)
-                return LocalRedirect(returnUrl ?? "/");
+    public IActionResult Login(string? returnUrl = null)
+    {
+        ViewBag.ReturnUrl = returnUrl;
 
-            ModelState.AddModelError(string.Empty, "Login ou senha inválidos");
+        return View();
+    }
 
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
+    {
+        ViewBag.ReturnUrl = returnUrl;
+
+        if (!ModelState.IsValid)
             return View(model);
-        }
 
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            await signInManager.SignOutAsync();
+        var resultadoLogin = await signInManager.PasswordSignInAsync(
+            model.Usuario,
+            model.Senha,
+            false,
+            false
+        );
 
-            return RedirectToAction(nameof(Login));
-        }
+        if (resultadoLogin.Succeeded)
+            return LocalRedirect(returnUrl ?? "/");
 
-        public IActionResult AcessoNegado()
-        {
-            return View();
-        }
+        ModelState.AddModelError(string.Empty, "Login ou senha inválidos");
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+
+        return RedirectToAction(nameof(Login));
+    }
+
+    public IActionResult AcessoNegado()
+    {
+        return View();
     }
 }
